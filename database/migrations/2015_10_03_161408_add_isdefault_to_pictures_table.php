40385faa55c35287afd2d7467fb434f853bdeb4f
Mehgan Cook <mehgancook@yahoo.com>
2015-10-03 09:40:02 -0700

    Added ability for default image

---
+++ database/migrations/2015_10_03_161408_add_isdefault_to_pictures_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsdefaultToPicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pictures', function (Blueprint $table) {
+            $table->boolean('is_default')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pictures', function (Blueprint $table) {
+            $table->dropColumn('is_default');
+        });
+    }
+}
