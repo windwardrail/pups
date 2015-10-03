fddedacac8b6ee6e89b26bdae0568e406559c40e
windwardrail <windwardrail@gmail.com>
2015-10-03 15:42:52 -0700

    Add laravel administrator and configuration for pet model

---
+++ config/administrator/pets.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+    'title' => 'Pet',
+
+    'single' => 'pet',
+
+    'model' => 'App\Pet',
+
+    'columns' => [
+        'name' => [
+            'title' => 'Name'
+        ],
+        'age' => [
+            'title' => 'Age'
+        ],
+        'breed' => [
+            'title' => 'Breed'
+        ],
+        'story' => [
+            'title' => 'Story',
+        ]
+     ],
+
+    'edit_fields' => [
+        'name' => [
+            'title' => 'Name',
+            'type' => 'text'
+        ],
+        'age' => [
+            'title' => 'Age',
+            'type' => 'number'
+        ],
+        'breed' => [
+            'title' => 'Breed',
+            'type' => 'text'
+        ],
+        'story' => [
+            'title' => 'Story',
+            'type' => 'text'
+        ]
+    ]
+
+
+);
--- config/app.php
+++ config/app.php
@@ -145,6 +145,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Frozennode\Administrator\AdministratorServiceProvider::class,
     ],
 
     /*
