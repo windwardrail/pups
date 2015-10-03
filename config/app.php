736fbdb1511a4213f9508e0521fdf208ddcb0ec4
anromeo <anthonykzullo@gmail.com>
2015-10-02 23:08:57 -0700

    added APP_KEY

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'azBY7AsMbSBBTI5lWEpdH4BcG7rDO1IO'),
 
     'cipher' => 'AES-256-CBC',
 
